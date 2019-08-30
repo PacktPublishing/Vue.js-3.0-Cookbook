31e597914191e9e0437ffa16c03cabcda59fe711
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-08-30 11:26:56 -0300

    Lint fix

--- chapter-2/2.4/src/store.js
+++ chapter-2/2.4/src/store.js
@@ -1,7 +1,7 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
@@ -12,5 +12,5 @@ export default new Vuex.Store({
   },
   actions: {
 
-  }
-})
+  },
+});
