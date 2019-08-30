e7c4f64edbca4597ed245bd9d347ce4428d86541
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-08-30 12:08:22 -0300

    Updates.

---
+++ chapter-2/2.3/Animal.ts
@@ -0,0 +1,40 @@
+export enum FoodChainType {
+  Carnivorous = 'carnivorous',
+  Herbivorous = 'herbivorous',
+  Omnivorous = 'omnivorous',
+}
+
+interface IBasicAnimal {
+  name: string;
+  sound?: string;
+  family: string;
+  foodChainType: FoodChainType;
+}
+
+interface IAnimalConstructor {
+  name: string,
+  sound?: string,
+  family: string,
+  foodChainType: FoodChainType
+}
+
+export class Animal implements IBasicAnimal {
+  public name: string;
+  public sound: string;
+  public family: string;
+  public foodChainType: FoodChainType;
+
+  constructor(params: IAnimalConstructor) {
+    this.name = params.name;
+    this.sound = params.sound || '';
+    this.family = params.family;
+    this.foodChainType = params.foodChainType;
+  }
+
+  public whoAmI() {
+    console.log(`I am a ${this.name}, my family is ${this.family}. My diet is ${this.foodChainType}.`);
+    if (this.sound) {
+      console.log(Array(2).fill(this.sound).join(', '));
+    }
+  }
+} 
---
+++ chapter-2/2.3/Dog.ts
@@ -0,0 +1,16 @@
+import {Animal, FoodChainType} from './Animal';
+
+class Dog extends Animal {
+  constructor() {
+    super({
+      name: 'Dog',
+      sound: 'Wof!',
+      family: 'Canidae',
+      foodChainType: FoodChainType.Carnivorous,
+    });
+  }
+
+  public bark() {
+    console.log(this.sound);
+  }
+}
