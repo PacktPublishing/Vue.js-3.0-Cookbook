2c57a50828066ba96dd2c34c77cab3b9e95e5354
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-09-05 05:25:15 -0300

    Updates

---
+++ chapter-2/2.11/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Counter
+      v-model="amount"
+    />
+    <hr />
+    <CounterByTen
+      v-model="amount"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Counter from './components/Counter.vue';
+import CounterByTen from './components/CounterByTen.vue';
+
+@Component({
+  components: {
+    Counter,
+    CounterByTen,
+  },
+})
+export default class App extends Vue {
+  amount: number = 0;
+}
+</script>
+<style lang="stylus">
+  #app
+    font-family 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    text-align center
+    color #2c3e50
+    margin-top 60px
+</style>
