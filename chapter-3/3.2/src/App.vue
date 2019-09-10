e2b883568aa3ce7ec3a8ad5ef7f202d20f11ea6f
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-09-09 23:40:46 -0300

    Updates

--- chapter-3/3.2/src/App.vue
+++ chapter-3/3.2/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <current-time class="col-4" />
+    <task-input class="col-6" />
   </div>
 </template>
 
 <script>
 import CurrentTime from './components/CurrentTime.vue';
+import TaskInput from './components/TaskInput';
 
 export default {
   name: 'app',
   components: {
-    CurrentTime
+    CurrentTime,
+    TaskInput,
   }
 }
 </script>
---
+++ chapter-3/3.2/src/components/TaskInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="cardBox">
+    <div class="container">
+      <h3>My task is:</h3>
+      <p>{{ task }}</p>
+      <hr />
+      <input type="text" v-model="task" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TaskInput',
+  data: () => ({
+    task: '',
+  }),
+};
+</script>
