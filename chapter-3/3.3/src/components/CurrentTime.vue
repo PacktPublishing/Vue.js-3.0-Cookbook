0245608aed6a1fcb7a01eea27324e45951d611e1
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-09-10 02:17:09 -0300

    updates

---
+++ chapter-3/3.3/src/components/CurrentTime.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="cardBox">
+    <div class="container">
+      <h1>Today is:</h1>
+      <h2>{{ getCurrentDate }}</h2>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "CurrentTime",
+  computed: {
+    getCurrentDate() {
+      const browserLocale =
+        navigator.languages && navigator.languages.length
+          ? navigator.languages[0]
+          : navigator.language;
+      const intlDateTime = new Intl.DateTimeFormat(browserLocale, {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric"
+      });
+
+      return intlDateTime.format(new Date());
+    }
+  }
+};
+</script>
