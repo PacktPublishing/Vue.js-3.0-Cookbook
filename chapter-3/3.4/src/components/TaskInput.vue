053de8272e77b8e5ef8c79d3def4fc7d169ece6d
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-09-12 23:23:17 -0300

    3.4

---
+++ chapter-3/3.4/src/components/TaskInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="cardBox">
+    <div class="container">
+      <h3>My task is:</h3>
+      <p>{{ task }}</p>
+      <hr />
+      <input type="text" v-model="task" />
+      <button
+        v-on:click="addTask"
+      >
+        Add Task
+      </button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TaskInput',
+  data: () => ({
+    task: '',
+  }),
+  methods: {
+    addTask(){
+      this.$emit('add-task', this.task);
+      this.task = '';
+    },
+  }
+};
+</script>
