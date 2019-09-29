7d31a0ccf57812e82e76ccd60ddd527c9c7a6995
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-09-29 02:10:07 -0300

    Updates

---
+++ chapter-4/4.1/src/components/MaterialCardBox.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="cardBox">
+    <div class="section">
+      This is a Material Card Box
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'MaterialCardBox',
+};
+</script>
+<style scoped>
+  .cardBox{
+    border-radius: 0.25rem;
+    box-shadow: 0 1px 5px rgba(0,0,0,0.2),
+    0 2px 2px rgba(0,0,0,0.14),
+    0 3px 1px -2px rgba(0,0,0,0.12);
+    background-color: #fff;
+    position: relative;
+  }
+  .cardBox > .section {
+    padding: 1rem;
+    position: relative;
+  }
+</style>
