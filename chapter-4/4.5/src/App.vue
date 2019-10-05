423edebb3f17d62dee29efd40537d521ba26ede8
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-10-05 19:14:11 -0300

    Updates

---
+++ chapter-4/4.5/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <MaterialCardBox
+      header="Material Card Header"
+      sub-header="Card Sub Header"
+      show-media
+      show-actions
+      img-src="https://picsum.photos/300/200"
+    >
+      <p>
+        <StarRating
+          ref="starRating"
+        />
+      </p>
+      <p>
+        The path of the righteous man is beset on all sides by the iniquities
+        of the selfish and the tyranny of evil men.
+      </p>
+      <template v-slot:action>
+        <MaterialButton
+          background-color="#027be3"
+          text-color="#fff"
+          @click="resetVote"
+        >
+          Reset
+        </MaterialButton>
+        <MaterialButton
+          background-color="#26a69a"
+          text-color="#fff"
+          is-flat
+          @click="forceVote"
+        >
+          Rate 5 Stars
+        </MaterialButton>
+      </template>
+    </MaterialCardBox>
+  </div>
+</template>
+
+<script>
+import MaterialCardBox from './components/MaterialCardBox.vue';
+import MaterialButton from './components/MaterialButton.vue';
+import StarRating from './components/StarRating.vue';
+
+export default {
+  name: 'App',
+  components: {
+    StarRating,
+    MaterialButton,
+    MaterialCardBox,
+  },
+  methods: {
+    resetVote() {
+      this.$refs.starRating.rating = 0;
+    },
+    forceVote() {
+      this.$refs.starRating.rating = 5;
+    },
+  },
+};
+</script>
+<style>
+  body {
+    font-size: 14px;
+  }
+</style>
---
+++ chapter-4/4.5/src/components/StarRating.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <StarRatingInput
+      v-if="!rating"
+      :max-rating="maxRating"
+      @final-vote="rating = $event"
+    >
+      Rate this Place
+    </StarRatingInput>
+    <StarRatingDisplay
+      v-else
+      :max-rating="maxRating"
+      :rating="rating"
+      :votes="votes"
+    />
+  </div>
+</template>
+
+<script>
+import StarRatingInput from './StarRatingInput.vue';
+import StarRatingDisplay from './StarRatingDisplay.vue';
+
+export default {
+  name: 'StarRating',
+  components: { StarRatingDisplay, StarRatingInput },
+  props: {
+    maxRating: {
+      type: Number,
+      required: false,
+      default: 5,
+    },
+    rating: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    votes: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+  },
+  data: () => ({
+    rating: 0,
+  }),
+};
+</script>
+
+<style scoped>
+  .starRating {
+    user-select: none;
+    display: flex;
+    flex-direction: row;
+  }
+
+  .starRating * {
+    line-height: 0.9rem;
+  }
+
+  .starRating .material-icons {
+    font-size: .9rem !important;
+    color: orange;
+  }
+
+  ul {
+    display: inline-block;
+    padding: 0;
+    margin: 0;
+  }
+
+  ul > li {
+    list-style: none;
+    float: left;
+  }
+
+  .rating, .votes {
+    display: inline-block;
+    color: rgba(0, 0, 0, .65);
+    font-size: .75rem;
+    margin-left: .4rem;
+  }
+</style>
