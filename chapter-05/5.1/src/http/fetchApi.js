2dc5ef4b02d5ba990c081f1966ab457e61c19666
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-10-16 00:40:36 -0300

    Updates

--- chapter-05/5.1/src/http/fetchApi.js
+++ chapter-05/5.1/src/http/fetchApi.js
@@ -1,28 +1,4 @@
-const baseHttp = async (url, method, options = {}) => {
-  let httpRequest;
-  if (method.toUpperCase() === 'GET') {
-    httpRequest = await fetch(url);
-  } else {
-    httpRequest = fetch(url, {
-      method: method.toUpperCase(),
-      ...options,
-    });
-  }
-  return (type) => {
-    switch (type.toLocaleLowerCase()) {
-      case 'json':
-        return httpRequest.json();
-      case 'blob':
-        return httpRequest.blob();
-      case 'text':
-        return httpRequest.text();
-      case 'formdata':
-        return httpRequest.formData();
-      default:
-        return httpRequest.arrayBuffer();
-    }
-  };
-};
+import baseHttp from './baseFetch';
 
 export const getHttp = async (url, type = 'json') => (await baseHttp(url, 'get'))(type);
 
