58a09a505edd192979502971d3d864b934da40ef
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-10-31 00:10:09 -0300

    Updates

---
+++ chapter-06/6.2/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <NavigationBar/>
+    <router-view/>
+  </div>
+</template>
+<script>
+import NavigationBar from './components/navigationBar.vue';
+
+export default {
+  components: { NavigationBar },
+  mounted() {
+    setTimeout(() => {
+      this.$router.push({
+        name: 'contact',
+        params: {
+          name: 'Heitor Ribeiro',
+          age: 31,
+        },
+      });
+    }, 5000);
+  },
+};
+</script>
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  #nav {
+    padding: 30px;
+  }
+
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
+
+  #nav a.router-link-exact-active {
+    color: #42b983;
+  }
+</style>
---
+++ chapter-06/6.2/src/views/contact.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="contact">
+    <h1>This is a contact page</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ContactPage',
+  mounted() {
+    if (Object.keys(this.$route.params).length) {
+      alert(`Hey! I've got some parameter! ${JSON.stringify(this.$route.params)}`);
+    }
+  },
+};
+</script>
