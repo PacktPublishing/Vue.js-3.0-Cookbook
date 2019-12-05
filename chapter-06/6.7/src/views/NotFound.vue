0e6532f5edd95235ecae2abcc098442d1e2bb5d9
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-12-05 02:41:51 -0300

    Updates

--- chapter-06/6.7/src/views/NotFound.vue
+++ chapter-06/6.7/src/views/NotFound.vue
@@ -1,26 +1,26 @@
 <template>
-    <vs-row>
-      <vs-col vs-w="12" class="text-center">
-        <vs-icon
-          icon="sentiment_dissatisfied"
-          size="large"
-        />
-      </vs-col>
-      <vs-col vs-w="12" class="text-center">
-        <h1>Ops!</h1>
-      </vs-col>
-      <vs-col vs-w="12" class="text-center">
-        <h3>The page you are looking for are not here anymore...</h3>
-      </vs-col>
-      <vs-col vs-w="12" class="text-center">
-        <vs-button
-          type="relief"
-          to="/"
-        >
-          Back to Home...
-        </vs-button>
-      </vs-col>
-    </vs-row>
+  <vs-row>
+    <vs-col vs-w="12" class="text-center">
+      <vs-icon
+        icon="sentiment_dissatisfied"
+        size="large"
+      />
+    </vs-col>
+    <vs-col vs-w="12" class="text-center">
+      <h1>Oops!</h1>
+    </vs-col>
+    <vs-col vs-w="12" class="text-center">
+      <h3>The page you are looking for are not here anymore...</h3>
+    </vs-col>
+    <vs-col vs-w="12" class="text-center">
+      <vs-button
+        type="relief"
+        to="/"
+      >
+        Back to Home...
+      </vs-button>
+    </vs-col>
+  </vs-row>
 </template>
 <script>
   export default {
