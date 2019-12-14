c6b382c1d8e8fe07cec1bc8335a30de93c0b136b
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-12-14 14:34:37 -0300

    Updates

--- chapter-07/7.2/src/store/index.js
+++ chapter-07/7.2/src/store/index.js
@@ -1,15 +1,9 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import UserStore from './user';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  ...UserStore,
 })
---
+++ chapter-07/7.2/src/store/user/index.js
@@ -0,0 +1,5 @@
+import state from './state';
+
+export default {
+  state,
+};
