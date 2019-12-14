2e443c68ba9ae9e8d95763c9e48f10b87e8f39e5
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-12-14 12:09:15 -0300

    Updates

---
+++ chapter-07/7.2/src/views/NotFound.vue
@@ -0,0 +1,35 @@
+<template>
+    <vs-row>
+      <vs-col vs-w="12" class="text-center">
+        <vs-icon
+          icon="sentiment_dissatisfied"
+          size="large"
+        />
+      </vs-col>
+      <vs-col vs-w="12" class="text-center">
+        <h1>Ops!</h1>
+      </vs-col>
+      <vs-col vs-w="12" class="text-center">
+        <h3>The page you are looking for are not here anymore...</h3>
+      </vs-col>
+      <vs-col vs-w="12" class="text-center">
+        <vs-button
+          type="relief"
+          to="/"
+        >
+          Back to Home...
+        </vs-button>
+      </vs-col>
+    </vs-row>
+</template>
+<script>
+  export default {
+    name: 'NotFound',
+  }
+</script>
+<style scoped>
+  .text-center {
+    text-align: center;
+    margin-bottom: 20px;
+  }
+</style>
