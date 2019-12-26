d8150e6082e173522f1f2ba4be7f683bcee80ef0
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-12-26 00:12:18 -0300

    Updates

--- chapter-07/7.7/src/store/index.js
+++ chapter-07/7.7/src/store/index.js
@@ -2,8 +2,33 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import UserStore from './user';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-export default new Vuex.Store({
+export const store = new Vuex.Store({
   ...UserStore,
-})
+});
+
+if (module.hot) {
+  const hmr = [
+    './user',
+    './user/getters',
+    './user/actions',
+    './user/mutations',
+  ];
+
+  const reloadCallback = () => {
+    const getters = require('./user/getters').default;
+    const actions = require('./user/actions').default;
+    const mutations =  require('./user/mutations').default;
+
+    store.hotUpdate({
+      getters,
+      actions,
+      mutations,
+    })
+  };
+
+  module.hot.accept(hmr, reloadCallback);
+}
+
+export default store;
