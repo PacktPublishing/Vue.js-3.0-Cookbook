fcd732769c2aae87f517cafdf638c89b8cbf43fd
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-12-26 23:03:16 -0300

    Updates

--- chapter-08/8.1/src/App.vue
+++ chapter-08/8.1/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">{{ $t('menu.home') }}</router-link> |
+      <router-link to="/about">{{ $t('menu.about') }}</router-link>
     </div>
     <router-view/>
   </div>
