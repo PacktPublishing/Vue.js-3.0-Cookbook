800c5874547a4771bad047ba63a834ea37e78c10
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2020-01-10 00:11:32 -0300

    Updates

---
+++ chapter-08/8.5/src/components/AsyncComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <pre>
+    {{ jsonData }}
+  </pre>
+</template>
+<script>
+export default {
+  name: 'AsyncComponent',
+  data: () => ({
+    jsonData: {},
+  }),
+  async mounted() {
+    this.jsonData = await this.fetchJson();
+  },
+  methods: {
+    async fetchJson() {
+      const userId = Math.round(Math.random());
+      const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+
+      return response.json();
+    },
+  },
+};
+</script>
