a2ca08ef5e634080fff4f855e12bcb0a230890dc
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2020-01-31 00:39:47 -0300

    Updates

---
+++ chapter-09/9.3/src/graphql/subscriptions.js
@@ -0,0 +1,199 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateConversationLink = /* GraphQL */ `
+  subscription OnCreateConversationLink($conversationLinkUserId: ID!) {
+    onCreateConversationLink(conversationLinkUserId: $conversationLinkUserId) {
+      id
+      user {
+        id
+        username
+        avatar {
+          bucket
+          region
+          key
+        }
+        conversations {
+          nextToken
+        }
+        messages {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      conversationLinkUserId
+      conversation {
+        id
+        messages {
+          nextToken
+        }
+        associated {
+          nextToken
+        }
+        name
+        members
+        createdAt
+        updatedAt
+      }
+      conversationLinkConversationId
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreateMessage = /* GraphQL */ `
+  subscription OnCreateMessage($messageConversationId: ID!) {
+    onCreateMessage(messageConversationId: $messageConversationId) {
+      id
+      author {
+        id
+        username
+        avatar {
+          bucket
+          region
+          key
+        }
+        conversations {
+          nextToken
+        }
+        messages {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      authorId
+      message
+      image {
+        bucket
+        region
+        key
+      }
+      conversation {
+        id
+        messages {
+          nextToken
+        }
+        associated {
+          nextToken
+        }
+        name
+        members
+        createdAt
+        updatedAt
+      }
+      messageConversationId
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreateUser = /* GraphQL */ `
+  subscription OnCreateUser($id: String!) {
+    onCreateUser(id: $id) {
+      id
+      username
+      avatar {
+        bucket
+        region
+        key
+      }
+      conversations {
+        items {
+          id
+          conversationLinkUserId
+          conversationLinkConversationId
+          createdAt
+          updatedAt
+        }
+        nextToken
+      }
+      messages {
+        items {
+          id
+          authorId
+          message
+          messageConversationId
+          createdAt
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateUser = /* GraphQL */ `
+  subscription OnUpdateUser($id: String!) {
+    onUpdateUser(id: $id) {
+      id
+      username
+      avatar {
+        bucket
+        region
+        key
+      }
+      conversations {
+        items {
+          id
+          conversationLinkUserId
+          conversationLinkConversationId
+          createdAt
+          updatedAt
+        }
+        nextToken
+      }
+      messages {
+        items {
+          id
+          authorId
+          message
+          messageConversationId
+          createdAt
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteUser = /* GraphQL */ `
+  subscription OnDeleteUser($id: String!) {
+    onDeleteUser(id: $id) {
+      id
+      username
+      avatar {
+        bucket
+        region
+        key
+      }
+      conversations {
+        items {
+          id
+          conversationLinkUserId
+          conversationLinkConversationId
+          createdAt
+          updatedAt
+        }
+        nextToken
+      }
+      messages {
+        items {
+          id
+          authorId
+          message
+          messageConversationId
+          createdAt
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
