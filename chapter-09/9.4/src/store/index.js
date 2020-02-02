73dfe84ad80d5099206df21c32c7bd7599b16e24
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2020-02-02 02:48:01 -0300

    Updates

--- chapter-09/9.4/src/store/index.js
+++ chapter-09/9.4/src/store/index.js
@@ -1,11 +1,15 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import user from './user';
+
 Vue.use(Vuex);
 
 export default function (/* { ssrContext } */) {
   const Store = new Vuex.Store({
-    modules: {},
+    modules: {
+      user,
+    },
     strict: process.env.DEV,
   });
 
