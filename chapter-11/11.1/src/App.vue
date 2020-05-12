bfedd7311bc20bfb119bd55c020f0deb186441be
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2020-05-12 11:54:22 -0300

    Updates

--- chapter-11/11.1/src/App.vue
+++ chapter-11/11.1/src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main-layout />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import MainLayout from './layouts/main.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    MainLayout,
   },
 };
 </script>
--- chapter-11/11.1/src/layouts/main.vue
+++ chapter-11/11.1/src/layouts/main.vue
@@ -1,30 +1,19 @@
 <template>
   <div>
-    <section class="section">
-      <div class="container">
-        <h1 class="title">Section</h1>
-        <h2 class="subtitle">
-          A simple container to divide your page into <strong>sections</strong>, like the one you're
-          currently reading
-        </h2>
-      </div>
-    </section>
-    <slot />
-    <footer class="footer">
-      <div class="content has-text-centered">
-        <p>
-          <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code
-          is licensed
-          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-          is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA
-          4.0</a>.
-        </p>
-      </div>
-    </footer>
+    <top-menu />
+    <slot/>
+    <footer-section />
   </div>
 </template>
 <script>
+import FooterSection from '../components/footer.vue';
+import TopMenu from '../components/topMenu.vue';
+
 export default {
   name: 'MainLayout',
+  components: {
+    TopMenu,
+    FooterSection,
+  },
 };
 </script>
