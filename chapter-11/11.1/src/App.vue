9a8d1fe7182f4503a709ee71aabf61359f7e512a
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2020-05-12 11:59:44 -0300

    Updates

--- chapter-11/11.1/src/App.vue
+++ chapter-11/11.1/src/App.vue
@@ -1,25 +1,21 @@
 <template>
-  <main-layout />
+  <main-layout>
+    <hero-section />
+    <user-form />
+  </main-layout>
 </template>
 
 <script>
 import MainLayout from './layouts/main.vue';
+import HeroSection from './components/heroSection.vue';
+import UserForm from './components/userForm.vue';
 
 export default {
   name: 'App',
   components: {
+    UserForm,
+    HeroSection,
     MainLayout,
   },
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ chapter-11/11.1/src/components/heroSection.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="hero is-primary">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          User Registration
+        </h1>
+        <h2 class="subtitle">
+          Main user registration form
+        </h2>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: 'HeroSection',
+};
+</script>
