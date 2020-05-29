c806a92fea192f14f2daad5b295437390eb5ed1f
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2020-05-28 23:02:38 -0300

    Updates

--- chapter-13/13.2/src/App.vue
+++ chapter-13/13.2/src/App.vue
@@ -1,60 +1,234 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
+  <layout>
+    <v-container
+      fluid
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+      <h1>User Registration</h1>
+      <v-subheader>Main user registration form</v-subheader>
+      <v-form
+        ref="form"
+        v-model="valid"
+        lazy-validation
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+        <v-container
+          fluid
+        >
+          <v-row>
+            <v-col
+              cols="12"
+            >
+              <v-card
+                outlined
+                flat
+                class="mx-auto"
+              >
+                <v-card-title>
+                  Personal Information
+                </v-card-title>
+                <v-divider/>
+                <v-container
+                  fluid
+                >
+                  <v-row>
+                    <v-col
+                      cols="12"
+                    >
+                      <v-text-field
+                        v-model="name"
+                        :rules="nameRules"
+                        label="Name"
+                        required
+                      />
+                    </v-col>
+                    <v-col
+                      cols="12"
+                    >
+                      <v-text-field
+                        v-model="email"
+                        :rules="emailRules"
+                        type="email"
+                        label="E-mail"
+                        required
+                      />
+                    </v-col>
+                    <v-col
+                      cols="6"
+                    >
+                      <v-text-field
+                        v-model="phone"
+                        label="Phone"
+                      />
+                    </v-col>
+                    <v-col
+                      cols="6"
+                    >
+                      <v-text-field
+                        v-model="cellphone"
+                        label="Cellphone"
+                      />
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card>
+            </v-col>
+            <v-col
+              cols="12"
+            >
+              <v-card
+                outlined
+                flat
+                class="mx-auto"
+              >
+                <v-card-title>
+                  Address
+                </v-card-title>
+                <v-divider/>
+                <v-container
+                  fluid
+                >
+                  <v-row>
+                    <v-col
+                      cols="12"
+                    >
+                      <v-text-field
+                        v-model="address"
+                        label="Address"
+                      />
+                    </v-col>
+                    <v-col
+                      cols="6"
+                    >
+                      <v-text-field
+                        v-model="zipcode"
+                        label="Zipcode"
+                      />
+                    </v-col>
+                    <v-col
+                      cols="6"
+                    >
+                      <v-text-field
+                        v-model="country"
+                        label="Country"
+                      />
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card>
+            </v-col>
+            <v-col
+              cols="12"
+            >
+              <v-card
+                outlined
+                flat
+                class="mx-auto"
+              >
+                <v-card-title>
+                  User Information
+                </v-card-title>
+                <v-divider/>
+                <v-container
+                  fluid
+                >
+                  <v-row>
+                    <v-col
+                      cols="6"
+                    >
+                      <v-text-field
+                        v-model="username"
+                        label="Username"
+                      />
+                    </v-col>
+                    <v-col
+                      cols="6"
+                    >
+                      <v-text-field
+                        v-model="password"
+                        :rules="passwordRules"
+                        label="Password"
+                        type="password"
+                        required
+                      />
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card>
+            </v-col>
+            <v-col
+              cols="12"
+              class="text-right"
+            >
+              <v-btn
+                color="error"
+                class="mr-4"
+                @click="cancel"
+              >
+                Cancel
+              </v-btn>
+              <v-btn
+                :disabled="!valid"
+                color="success"
+                class="mr-4"
+                @click="register"
+              >
+                Register
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-form>
+    </v-container>
+  </layout>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Layout from './components/Layout.vue';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Layout,
   },
 
   data: () => ({
-    //
+    valid: true,
+    name: '',
+    username: '',
+    password: '',
+    email: '',
+    phone: '',
+    cellphone: '',
+    address: '',
+    zipcode: '',
+    country: '',
   }),
+  computed: {
+    nameRules() {
+      return [
+        (v) => !!v || 'Name is required',
+      ];
+    },
+    passwordRules() {
+      return [
+        (v) => !!v || 'Password is required',
+      ];
+    },
+    emailRules() {
+      return [
+        (v) => !!v || 'E-mail is required',
+        (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ];
+    },
+  },
+
+  methods: {
+    register() {
+      this.$refs.form.validate();
+    },
+    cancel() {
+      this.$refs.form.reset();
+    },
+  },
 };
 </script>
