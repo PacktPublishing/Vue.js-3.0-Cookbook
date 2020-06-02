e9e48853e8444063c68aaf5ebbbc48e1404911b7
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2020-06-02 01:43:24 -0300

    Updates

--- chapter-13/13.3/src/App.vue
+++ chapter-13/13.3/src/App.vue
@@ -1,6 +1,63 @@
 <template>
   <layout>
-    User Form
+    <a-form-model
+      :model="form"
+      :label-col="labelCol"
+      :wrapper-col="wrapperCol"
+    >
+      <h1>
+        User Registration
+      </h1>
+      <p>Main user registration form</p>
+      <a-card title="Personal Information">
+        <a-form-model-item label="Name">
+          <a-input v-model="form.name" />
+        </a-form-model-item>
+        <a-form-model-item label="Email">
+          <a-input
+            v-model="form.email"
+            type="email"
+          />
+        </a-form-model-item>
+        <a-form-model-item label="Phone">
+          <a-input v-model="form.phone" />
+        </a-form-model-item>
+        <a-form-model-item label="Cellphone">
+          <a-input v-model="form.cellphone" />
+        </a-form-model-item>
+      </a-card>
+      <a-card title="Address" :style="{ marginTop: '16px' }">
+        <a-form-model-item label="Address">
+          <a-input v-model="form.address" />
+        </a-form-model-item>
+        <a-form-model-item label="Zipcode">
+          <a-input v-model="form.zipcode" />
+        </a-form-model-item>
+        <a-form-model-item label="Country">
+          <a-input v-model="form.country" />
+        </a-form-model-item>
+      </a-card>
+      <a-card title="User Information" :style="{ marginTop: '16px' }">
+        <a-form-model-item label="Username">
+          <a-input v-model="form.username" />
+        </a-form-model-item>
+        <a-form-model-item label="Password">
+          <a-input-password
+            v-model="form.password"
+            visibility-toggle
+            type="password"
+          />
+        </a-form-model-item>
+      </a-card>
+      <a-form-model-item :wrapper-col="{ span: 14, offset: 4 }">
+        <a-button type="primary">
+          Create
+        </a-button>
+        <a-button style="margin-left: 10px;">
+          Cancel
+        </a-button>
+      </a-form-model-item>
+    </a-form-model>
   </layout>
 </template>
 
@@ -11,6 +68,24 @@ export default {
   name: 'app',
   components: {
     Layout,
+  },
+  data() {
+    return {
+      labelCol: { span: 4 },
+      wrapperCol: { span: 14 },
+      form: {
+        valid: true,
+        name: '',
+        username: '',
+        password: '',
+        email: '',
+        phone: '',
+        cellphone: '',
+        address: '',
+        zipcode: '',
+        country: '',
+      },
+    };
   },
 };
 </script>
