a8a13ad3ccba978379f118228a46b92877668203
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2020-06-06 00:15:15 -0300

    Updates

--- chapter-13/13.3/src/components/Layout.vue
+++ chapter-13/13.3/src/components/Layout.vue
@@ -6,7 +6,7 @@
     <a-layout>
       <top-bar />
       <a-layout-content style="margin: 0 16px">
-        <div :style="{ padding: '24px', background: '#fff', minHeight: 'auto' }">
+        <div style="padding: 24px; background: #fff; min-height: auto;">
           <slot />
         </div>
       </a-layout-content>
