4d08b259d480508195b4094626d36c242bc55ee8
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2020-06-28 22:57:34 -0300

    Updates

--- chapter-12/12.2/src/App.vue
+++ chapter-12/12.2/src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <transition
+      enter-active-class="animated bounceInLeft"
+      leave-active-class="animated bounceOutLeft"
+    >
+      <img alt="Vue logo" src="./assets/logo.png">
+    </transition>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
+  data: () => ({
+    display: true,
+  }),
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- chapter-12/12.7/src/App.vue
+++ chapter-12/12.7/src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
+  data: () => ({
+    display: true,
+  }),
 };
 </script>
 
