2eb06585b6ec026650ddb66d7d5aed9094fa7156
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2020-07-01 21:40:26 -0300

    Updates

--- chapter-12/12.3/src/App.vue
+++ chapter-12/12.3/src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <div id="app">
-    <img
-      v-if="display"
-      alt="Vue logo" src="./assets/logo.png">
+    <transition
+      enter-active-class="animated rotateIn"
+      leave-active-class="animated rotateOut"
+      @after-enter="onEnter"
+      @after-leave="onLeave"
+    >
+      <img
+        v-if="display"
+        alt="Vue logo" src="./assets/logo.png">
+    </transition>
+    <h1>The image {{ status }}</h1>
     <button
       @click="display = !display"
     >
@@ -16,17 +24,26 @@ export default {
   name: 'App',
   data: () => ({
     display: true,
+    status: 'appeared',
   }),
+  methods: {
+    onEnter() {
+      this.status = 'appeared';
+    },
+    onLeave() {
+      this.status = 'disappeared';
+    },
+  },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
