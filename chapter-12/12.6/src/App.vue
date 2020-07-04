1b9aa2dcb9d2d1c1dd2e9d7976a02a7a21a25c60
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2020-07-03 22:12:02 -0300

    Updates

--- chapter-12/12.6/src/App.vue
+++ chapter-12/12.6/src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
-    <img
-      v-if="display"
-      alt="Vue logo" src="./assets/logo.png">
+    <custom-transition>
+      <img
+        v-if="display"
+        alt="Vue logo" src="./assets/logo.png">
+    </custom-transition>
     <button
       @click="display = !display"
     >
@@ -12,8 +14,13 @@
 </template>
 
 <script>
+import CustomTransition from './components/CustomTransition.vue';
+
 export default {
   name: 'App',
+  components: {
+    CustomTransition,
+  },
   data: () => ({
     display: true,
   }),
@@ -21,12 +28,12 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ chapter-12/12.6/src/components/CustomTransition.vue
@@ -0,0 +1,9 @@
+<template functional>
+  <transition
+    :appear="props.appear"
+    enter-active-class="animated slideInLeft"
+    leave-active-class="animated slideOutRight"
+  >
+    <slot />
+  </transition>
+</template>
