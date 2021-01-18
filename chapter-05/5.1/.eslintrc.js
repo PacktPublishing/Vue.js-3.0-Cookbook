ce0b7c3c91056a56a0f13ec9c9d95f265de975cb
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-01-18 11:58:52 -0300

    Updates

--- chapter-05/5.1/.eslintrc.js
+++ chapter-05/5.1/.eslintrc.js
@@ -4,15 +4,14 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/vue3-essential',
     '@vue/airbnb',
   ],
-  rules: {
-    'class-methods-use-this': 'off',
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
   parserOptions: {
     parser: 'babel-eslint',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
 };
